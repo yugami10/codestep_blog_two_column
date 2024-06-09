397c2345146011a4c8c0b02d0f9ce20d2161a32c
yugami10 <yugami333@gmail.com>
2024-06-09 14:27:46 +0900

    月別一覧16章

---
+++ date.php
@@ -0,0 +1,47 @@
+<?= get_header() ?>
+
+<div id="container" class="wrapper">
+    <main>
+        <h1 class="page-title">
+            <?= get_query_var('year') . '年' . get_query_var('monthnum') . '月' ?>
+        </h1>
+
+        <?php if(have_posts()): ?>
+            <?php while(have_posts()): the_post(); ?>
+                <?php
+                    $cat = get_the_category();
+                    $catname = $cat[0]->cat_name;
+                ?>
+                <article>
+                    <h2 class="article-title">
+                        <a href="<?= the_permalink() ?>"><?= the_title() ?></a>
+                    </h2>
+                    <ul class="meta">
+                        <li><?= the_time('Y/m/d') ?></li>
+                        <li><?= $catname ?></li>
+                    </ul>
+                    <a href="<?= the_permalink() ?>"><?= the_post_thumbnail() ?></a>
+                    <div class="text">
+                        <?php
+                            if (mb_strlen(strip_tags(get_the_content()), 'UTF-8') > 80) {
+                                $content = mb_substr(strip_tags(get_the_content()), 0, 80, 'UTF-8');
+                                echo $content . '…';
+                            } else {
+                                echo strip_tags(get_the_content());
+                            }
+                        ?>
+                    </div>
+                    <div class="readmore">
+                        <a href="<?= the_permalink() ?>">READ MORE</a>
+                    </div>
+                </article>
+            <?php endwhile; ?>
+
+            <?= pagination($wp_query->max_num_pages) ?>
+        <?php endif; ?>
+    </main>
+
+    <?= get_sidebar() ?>
+</div>
+
+<?= get_footer() ?>
